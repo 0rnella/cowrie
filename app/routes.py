89666f253f4ec4c5bec0c3775ab898ef8d38729e
friggito <ornella.friggit@gmail.com>
2019-07-23 12:23:48 -0400

    deleted front-end to separate into 2 repos

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,17 @@
 from app import app
 from flask import jsonify
 from flask import request
-from app.yelp_link_mbe import is_mbe_certified
+from .yelp_link_mbe import is_mbe_certified
 
 @app.route('/', methods=['GET'])
-def ping_pong():
-    return jsonify('pong!')
+def user():
+    return jsonify({'user': 'Ornella'})
 
 @app.route('/yelpcheck', methods=['GET'])
 def yelp_check():
     yelp_link = request.args.get('yelplink')
-    return jsonify(is_mbe_certified(yelp_link))
+    matches = []
+    if (yelp_link != None):
+        matches = is_mbe_certified(yelp_link)
+    return jsonify(matches)
 
